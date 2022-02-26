85710249b70af556ace2184252758118085a813b
Long Lu <longplu@Mac-mini.local>
2022-02-26 12:56:22 -0500

    wip: finishes Week 20 Day 1 instructions

--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+class Dog:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Lolo', 'tabby', 'foul little demon', 3),
+  Dog('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Dog('Raven', 'black tripod', '3 legged cat', 4)
+]
 
 def home(request):
     return HttpResponse('<h1>Hello</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def dogs_index(request):
+    return render(request, 'dogs/index.html', { 'dogs': dogs })
